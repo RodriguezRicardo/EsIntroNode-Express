1cbec500a2a03984407f8e16f4c0825a6220ad42
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2020-11-05 10:17:02 +0000

    01.1 Send static files with Express

---
+++ EXPRESS_STATIC_FILES/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express(); 
+
+const path = require('path')
+
+//modifica la chiamata alla funzione get di Express
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,'index.html'));  // ritorna la cartella del progetto(__dirname)
+});
+
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname,'about.html'));           //metodo get per raggiungere la pagina about.html
+});
+
+app.get('/sitemap', function(req,res){
+    res.sendFile(path.join(__dirname,'sitemap.html'));        //metodo get per raggiungere la pagina sitemap.html
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
